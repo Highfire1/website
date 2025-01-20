9340f29971004739e524b2055ef8eba6359a3b4c
Anderson T <tseng.andersonn@gmail.com>
2025-01-20 01:34:54 -0800

    better build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+
 const nextConfig: NextConfig = {
-  output: "export",
+  ...(isGithubActions && {
+    output: "export"
+  }),
   images: {
     unoptimized: true
   }
